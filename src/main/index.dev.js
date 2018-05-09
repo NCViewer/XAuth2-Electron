1bf80839d3babffc8e4300b779fb651155061673
Xander Luciano <xanderluciano@gmail.com>
2018-05-09 04:08:14 -0700

    fixin' stuff I think.

--- src/main/index.dev.js
+++ src/main/index.dev.js
@@ -8,20 +8,25 @@
 /* eslint-disable */
 
 // Set environment for development
-process.env.NODE_ENV = 'development'
+process.env.NODE_ENV = 'development';
 
-// Install `electron-debug` with `devtron`
-require('electron-debug')({ showDevTools: false })
+import electron from 'electron';
+import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer';
+
+// Enable dev tools, but don't automatically show
+import electronDebug from 'electron-debug'
+electronDebug({ showDevTools: false });
 
 // Install `vue-devtools`
-require('electron').app.on('ready', () => {
-  let installExtension = require('electron-devtools-installer')
-  installExtension.default(installExtension.VUEJS_DEVTOOLS)
-    .then(() => {})
-    .catch(err => {
-      console.log('Unable to install `vue-devtools`: \n', err)
-    })
-})
+electron.app.on('ready', () => {
+
+    // let installExtension = require('electron-devtools-installer');
+    installExtension(VUEJS_DEVTOOLS)
+        .then( (name) => console.log(`Added Extension: ${name}`) )
+        .catch( (err) => {
+            console.log('Unable to install `vue-devtools`: \n', err);
+        });
+});
 
 // Require `main` process to boot app
-require('./index')
+require('./index');
